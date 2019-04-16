16f97a3b48e0679f24f9de5e3751bf40b556a6e3
Jan Macenka <36518907+DonJanosch@users.noreply.github.com>
2019-04-16 20:35:54 +0200

    Update install_node_red.sh

--- install_node_red.sh
+++ install_node_red.sh
@@ -4,4 +4,5 @@ systemctl enable nodered.service
 git clone https://github.com/DonJanosch/node-red-flows.git flows && cd flows && bash unpack_libraries.sh
 cd ~/.node-red && npm install node-red-contrib-state-machine node-red-dashboard node-red-contrib-opcua
 echo "Successfully finished the Node-Red-Installer-Script, rebooting now. The System will be avaliable under http://"$(hostname -I | cut -d' ' -f1)":1880 after the reboot."
+echo "WARNING: Be aware that this installation-script leaves you with an OPEN and UNPROTECTED installation of Node-Red and requires you to handle soch measures manually afterwards!"
 reboot
