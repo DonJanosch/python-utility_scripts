ebf4fca5591af53a8405dc58178ab8d42dd7b6be
Jan Macenka <36518907+DonJanosch@users.noreply.github.com>
2019-04-16 20:14:47 +0200

    Update install_node_red.sh

--- install_node_red.sh
+++ install_node_red.sh
@@ -1,9 +1,8 @@
 apt update && apt upgrade -y
-apt install git curl
 bash <(curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered)
 systemctl enable nodered.service
-git clone https://github.com/DonJanosch/node-red-flows.git .
-bash unpack_libraries.sh
+git clone https://github.com/DonJanosch/node-red-flows.git flows
+bash flows/unpack_libraries.sh
 cd ~/.node-red
 npm install node-red-contrib-state-machine node-red-dashboard node-red-contrib-opcua
 reboot
