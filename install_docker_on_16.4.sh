3720ff6935052b4b7b6a04dc5cbab64462e8a948
Jan Macenka <36518907+DonJanosch@users.noreply.github.com>
2019-06-04 23:40:53 +0200

    Update install_docker_on_16.4.sh

--- install_docker_on_16.4.sh
+++ install_docker_on_16.4.sh
@@ -1,5 +1,4 @@
 echo "Trying to install docker, requires root-privileges and will add current user to docker-group."
 bash <(curl https://get.docker.com)
 eval "sudo usermod -aG docker $(whoami)"
-echo "Installed docker, restarting now"
-reboot
+echo "Installed docker"
