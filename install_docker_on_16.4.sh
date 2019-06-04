5f16f75959c7918b8bcfea9c4d54ac7eaa9438e3
Jan Macenka <36518907+DonJanosch@users.noreply.github.com>
2019-06-04 23:57:20 +0200

    Update install_docker_on_16.4.sh

--- install_docker_on_16.4.sh
+++ install_docker_on_16.4.sh
@@ -1,4 +1,8 @@
 echo "Trying to install docker, requires root-privileges and will add current user to docker-group."
 bash <(curl https://get.docker.com)
 eval "sudo usermod -aG docker $(whoami)"
-echo "Installed docker"
+echo "Installed docker: $(docker --version)"
+echo "Trying to install docker-compose."
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+echo "Installed docker-compose: &(docker-compose --version)"
