93d7b507d037b853b25d4be6c0073fb26b93be33
Jan Macenka <36518907+DonJanosch@users.noreply.github.com>
2019-06-04 11:31:39 +0200

    removed typeo in installerscript for flask qrcode-server

--- start_qrcode_server.sh
+++ start_qrcode_server.sh
@@ -4,6 +4,6 @@ docker rm -f $(docker ps -a -q)
 docker rmi $(docker images -q)
 git clone https://github.com/DonJanosch/flask-qrgenerator.git app
 cd app
-docker dbuild -t flask-qrgenerator:latest .
+docker build -t flask-qrgenerator:latest .
 docker run -d -p 80:8080 --restart always --name qrgenerator flask-qrgenerator
 echo "All Done, Container is up and running"
