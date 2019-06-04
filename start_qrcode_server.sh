478300f51ecbf35c3725a9df0a21f749b60d2e27
Jan Macenka <36518907+DonJanosch@users.noreply.github.com>
2019-06-04 11:35:27 +0200

    Changed script for flask-qrcode-server to a oneliner

--- start_qrcode_server.sh
+++ start_qrcode_server.sh
@@ -1,9 +1,1 @@
-cd ~
-rm -r app
-docker rm -f $(docker ps -a -q)
-docker rmi $(docker images -q)
-git clone https://github.com/DonJanosch/flask-qrgenerator.git app
-cd app
-docker build -t flask-qrgenerator:latest .
-docker run -d -p 80:8080 --restart always --name qrgenerator flask-qrgenerator
-echo "All Done, Container is up and running"
+cd ~ && rm -r app && docker rm -f $(docker ps -a -q) && docker rmi $(docker images -q) && git clone https://github.com/DonJanosch/flask-qrgenerator.git app && cd app && docker dbuild -t && flask-qrgenerator:latest . && docker run -d -p 80:8080 --restart always --name qrgenerator flask-qrgenerator && echo "All Done, Container is up and running"
