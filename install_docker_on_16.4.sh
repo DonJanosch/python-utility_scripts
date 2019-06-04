0b4d9155f59a8923f09788fda71b6858c0836642
Jan Macenka <36518907+DonJanosch@users.noreply.github.com>
2019-06-05 00:00:33 +0200

    Update install_docker_on_16.4.sh

--- install_docker_on_16.4.sh
+++ install_docker_on_16.4.sh
@@ -5,4 +5,4 @@ echo "Installed docker: $(docker --version)"
 echo "Trying to install docker-compose."
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-echo "Installed docker-compose: &(docker-compose --version)"
+echo "Installed docker-compose: $(docker-compose --version)"
