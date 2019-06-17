2f1aa1320eed0c8146010deffc45402b2e655762
Jan Macenka <36518907+DonJanosch@users.noreply.github.com>
2019-06-17 09:15:18 +0200

    Added line to start docker service under systemctl

--- install_docker_on_16.4.sh
+++ install_docker_on_16.4.sh
@@ -1,6 +1,7 @@
 echo "Trying to install docker, requires root-privileges and will add current user to docker-group."
 bash <(curl https://get.docker.com)
 eval "sudo usermod -aG docker $(whoami)"
+sudo systemctl start docker
 echo "Installed docker: $(docker --version)"
 echo "Trying to install docker-compose."
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
