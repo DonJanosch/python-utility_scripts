e122c5333a749d5073dcb55a31eeda4db50bbc8f
Jan Macenka <36518907+DonJanosch@users.noreply.github.com>
2019-08-27 22:42:41 +0200

    Update setup_ubuntu_18.04.sh

--- setup_ubuntu_18.04.sh
+++ setup_ubuntu_18.04.sh
@@ -1,12 +1,16 @@
+sudo apt update
+sudo apt install software-properties-common curl flatpak -y
+
 cd ~/Downloads
 sudo wget -qnc https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
 sudo dpkg -i ~/Downloads/nordvpn-release_1.0.0_all.deb
 sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
+sudo add-apt-repository -y ppa:projectatomic/ppa
 
 sudo apt update
 snap install postman
 snap install atom --classic
-sudo apt install curl flatpak nordvpn mate-desktop-environment git python3.7 -y
+sudo apt install podman nordvpn mate-desktop-environment git python3.7 -y
 
 curl https://get.docker.com | sudo bash
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
