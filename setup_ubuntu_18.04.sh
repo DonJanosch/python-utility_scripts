b3d2212aef6092e17330ed2ef905b282b81bfe7a
Jan Macenka <36518907+DonJanosch@users.noreply.github.com>
2019-08-27 22:28:24 +0200

    Add files via upload

---
+++ setup_ubuntu_18.04.sh
@@ -0,0 +1,19 @@
+cd ~/Downloads
+sudo wget -qnc https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
+sudo dpkg -i ~/Downloads/nordvpn-release_1.0.0_all.deb
+sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
+
+sudo apt update
+snap install postman
+snap install atom --classic
+sudo apt install curl flatpak nordvpn mate-desktop-environment git python3.7 -y
+
+curl https://get.docker.com | sudo bash
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
+sudo apt autoremove -y
+sudo apt autoclean -y
