b478083113789f7e826a05706774a6547a552637
Jan Macenka <36518907+DonJanosch@users.noreply.github.com>
2019-08-27 22:52:33 +0200

    Update setup_ubuntu_18.04.sh

--- setup_ubuntu_18.04.sh
+++ setup_ubuntu_18.04.sh
@@ -4,13 +4,17 @@ sudo apt install software-properties-common curl flatpak -y
 cd ~/Downloads
 sudo wget -qnc https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
 sudo dpkg -i ~/Downloads/nordvpn-release_1.0.0_all.deb
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
+sudo add-apt-repository "deb [arch=i386,amd64] https://deb.opera.com/opera-stable/ stable non-free"
 sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
 sudo add-apt-repository -y ppa:projectatomic/ppa
 
 sudo apt update
 snap install postman
 snap install atom --classic
-sudo apt install podman nordvpn mate-desktop-environment git python3.7 -y
+sudo apt install podman nordvpn mate-desktop-environment git python3.7 google-chrome-stable opera-stable -y
 
 curl https://get.docker.com | sudo bash
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
