cee2d276a65283b49e18e3cd16d52aec20ec6770
Jan Macenka <jan.macenka@gmail.com>
2020-03-03 21:05:39 +0100

    Updated PLEX install script

--- install_plex.sh
+++ install_plex.sh
@@ -8,12 +8,13 @@ echo "Installing https-transport..."
 sudo apt-get install apt-transport-https 
 
 echo "Adding repo-keys..."
-wget -O - https://dev2day.de/pms/dev2day-pms.gpg.key | sudo apt-key add -
-echo "deb https://dev2day.de/pms/ jessie main" | sudo tee /etc/apt/sources.list.d/pms.list
+
+curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
+echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
 sudo apt-get update
 
 echo "Installing PLEX Server..."
-sudo apt-get install -t jessie plexmediaserver
+sudo apt-get install plexmediaserver
 
 echo "Adapting settings of PLEX Server for pi and restarting the service..."
 sed -i -e 's/PLEX_MEDIA_SERVER_USER=plex/PLEX_MEDIA_SERVER_USER=pi/g' /etc/default/plexmediaserver.prev
