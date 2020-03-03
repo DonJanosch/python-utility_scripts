efbf717005fa69ef4721ad0d306f0a40879cc7d4
Jan Macenka <jan.macenka@gmail.com>
2020-03-03 21:20:16 +0100

    Minor changes

--- install_plex.sh
+++ install_plex.sh
@@ -8,7 +8,6 @@ echo "Installing https-transport..."
 sudo apt-get install apt-transport-https 
 
 echo "Adding repo-keys..."
-
 curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
 echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
 sudo apt-get update
