25e81f9421a43b1fbae29a21804f7bba2f75fd79
Jan Macenka <36518907+jmacenka@users.noreply.github.com>
2020-03-12 23:27:41 +0100

    Update setup_visual_studio_code_ubuntu.sh

--- setup_visual_studio_code_ubuntu.sh
+++ setup_visual_studio_code_ubuntu.sh
@@ -1,5 +1,5 @@
 # Script Settings
-SETTINGS_PATH=~/.config/Code/User/
+SETTINGS_PATH=$HOME/.config/Code/User/
 SETTINGS_FILENAME=settings.json
 
 # Scipt body
@@ -7,12 +7,7 @@ pkgs='code'
 if ! dpkg -s $pkgs >/dev/null 2>&1;
 then
     echo "Installing VisualStudio Code on Ubuntu for Jan Macenka"
-    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-    sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
-    sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-    sudo apt-get install apt-transport-https
-    sudo apt-get update
-    sudo apt-get install code
+    sudo snap install --classic code # or code-insiders
 else
     echo "VisualStudio Code seems to be installed already."
 fi
