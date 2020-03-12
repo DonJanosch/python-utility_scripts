5cc1a9f656e797e982663cf401e14fe61db7f35d
Jan Macenka <jan.macenka@gmail.com>
2020-03-12 23:06:56 +0100

    Added script to install VS Code extensions

---
+++ setup_visual_studio_code_ubuntu.sh
@@ -0,0 +1,30 @@
+# Script Settings
+SETTINGS_PATH=~/.config/Code/User/
+SETTINGS_FILENAME=settings.json
+
+# Scipt body
+pkgs='code'
+if ! dpkg -s $pkgs >/dev/null 2>&1;
+then
+    echo "Installing VisualStudio Code on Ubuntu for Jan Macenka"
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+    sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+    sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+    sudo apt-get install apt-transport-https
+    sudo apt-get update
+    sudo apt-get install code
+else
+    echo "VisualStudio Code seems to be installed already."
+fi
+
+echo "Installing VisualStudio Code Extensions for Jan Macenka"
+
+curl https://raw.githubusercontent.com/jmacenka/vscode-extensions/master/extensions.list | grep -v '^#' | xargs -L1 code --install-extension 
+
+echo "Pulling settings.json into '$SETTINGS_PATH$SETTINGS_FILENAME' for Jan Macenka"
+
+curl https://raw.githubusercontent.com/jmacenka/vscode-extensions/master/settings.json >> $SETTINGS_PATH
+
+echo "All done. Opening VS Code."
+
+code $SETTINGS_PATH
