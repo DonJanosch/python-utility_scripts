1b8e298b09ccaf5b69e9f286c58f6b419c3c297d
Jan Macenka <36518907+jmacenka@users.noreply.github.com>
2020-03-23 20:06:41 +0100

    Update setup_ubuntu_18.04.sh

--- setup_ubuntu_18.04.sh
+++ setup_ubuntu_18.04.sh
@@ -8,10 +8,12 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
 sudo add-apt-repository -y ppa:projectatomic/ppa
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+echo "deb https://dl.bintray.com/resin-io/debian stable etcher" | sudo tee/etc/apt/sources.list.d/etcher.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
 
 sudo apt update
 snap install postman
-sudo apt install brave-browser podman nordvpn mate-desktop-environment git google-chrome-stable opera-stable torbrowser-launcher -y
+sudo apt install brave-browser etcher-electron podman nordvpn git google-chrome-stable opera-stable torbrowser-launcher -y
 
 curl https://get.docker.com | sudo bash
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
