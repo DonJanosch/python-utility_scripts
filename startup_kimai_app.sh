ee01bd6476bcc1b9b75d8f4de0110c06feaa46f6
Jan Macenka <36518907+jmacenka@users.noreply.github.com>
2020-04-07 14:38:09 +0200

    Added pulling of local copy of starup-script

--- startup_kimai_app.sh
+++ startup_kimai_app.sh
@@ -2,6 +2,7 @@ PUBLISHED_PORT=8001
 
 mkdir app && cd $_
 curl https://raw.githubusercontent.com/jmacenka/docker_compose_apps/master/kimai_docker-compose.yaml >> docker-compose.yaml
+curl https://raw.githubusercontent.com/jmacenka/utility-scripts/master/startup_kimai_app.sh >> startup.sh
 
 docker-compose up -build --force-recreate
 echo "All Done, Containers are up and running. Visite http://$(hostname -I | cut -d' ' -f1):$PUBLISHED_PORT"
