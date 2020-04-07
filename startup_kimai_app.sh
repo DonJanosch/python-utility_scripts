b41aae1aa8d5f7b9817014efb35c2bf8ad54d051
Jan Macenka <36518907+jmacenka@users.noreply.github.com>
2020-04-07 14:39:23 +0200

    Update startup_kimai_app.sh

--- startup_kimai_app.sh
+++ startup_kimai_app.sh
@@ -1,6 +1,6 @@
 PUBLISHED_PORT=8001
 
-mkdir app && cd $_
+mkdir app && cd app
 curl https://raw.githubusercontent.com/jmacenka/docker_compose_apps/master/kimai_docker-compose.yaml >> docker-compose.yaml
 curl https://raw.githubusercontent.com/jmacenka/utility-scripts/master/startup_kimai_app.sh >> startup.sh
 
