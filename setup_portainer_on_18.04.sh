5710fa8018c593c386c2696c6c37ec7aa678f2e6
Jan Macenka <jan.macenka@gmail.com>
2020-08-01 21:37:02 +0200

    Fixed syntax error

--- setup_portainer_on_18.04.sh
+++ setup_portainer_on_18.04.sh
@@ -4,7 +4,6 @@
 # Scipt body
 # Information
 echo "Script for installing Docker CE and Portainer to a Debian like Ubuntu 18.04"
-echo "This is not a production great script! It will leave your installation vurnerable as it will not take care of any security precautions!"
 
 # Instaling Docker
 # First, update your existing list of packages:
@@ -18,16 +17,18 @@ sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubun
 # Next, update the package database with the Docker packages from the newly added repo:
 sudo apt update
 # Finally, install Docker:
-sudo apt install docker-ce
+sudo apt install -y docker-ce
 # Adding current user to docker group
 sudo usermod -aG docker ${USER}
+# Install portainer
 # Prepare portainer
 docker volume create portainer_data
 # Deploy portainer
 docker run -d -p 9000:9000 -p 8000:8000 --name portainer --restart always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
 
+# Final messages
+echo "This is not a production great script! It will leave your installation vurnerable as it will not take care of any security precautions!"
 if [ $(id -u) = 0 ]; then
    echo "I have detected that u installed this script as ROOT-User which is a bad idea! This is only a test setup. Go talk to your trusted IoT-advisor of choice!" 
 fi
-
 echo "All Done, Portainer is up and running. Visite http://$(hostname -I | cut -d' ' -f1):9000"
