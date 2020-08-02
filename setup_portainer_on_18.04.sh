7676e3b842e13644cf0f8e72ca59d493de8e55c5
Jan Macenka <jan.macenka@gmail.com>
2020-08-02 10:03:02 +0200

    Fixed some more typos

--- setup_portainer_on_18.04.sh
+++ setup_portainer_on_18.04.sh
@@ -4,9 +4,9 @@
 
 # Scipt body
 # INFORMATION
-# This script may look more impressing than it is. I just googled "how to install Docker on 18.04" and "how to deploy Portainer"
+# This script may look more impressing than it is. I just googled "how to install Docker on 18.04" and "how to deploy portainer"
 # Here I foud these results "https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-18-04" and "https://portainer.readthedocs.io/en/stable/deployment.html"
-# Then I literally just Copy&Pasted the tutorials there into this unified shell-script and uploaded it to GitHub for easy access and added some comment lines... thats it.
+# Then I literally just Copy&Pasted the tutorials there into this unified shell-script, added some comment lines and uploaded it to GitHub for easy access... thats it.
 # Everyone whith experience in IT, especially OpenSource and/or Linux should be able to do the same. Thats no special skill. Combined with an engineering-
 # spirit and the general ability to read thats all you need ;-) Go ask the Linux and/or IoT Comunity for help. They are great!
 echo "Script for installing Docker CE and Portainer to a Debian like Ubuntu 18.04"
@@ -38,6 +38,6 @@ echo ""
 echo "*************************"
 echo "This is not a production great script! It will leave your installation vurnerable as it will not take care of any security precautions!"
 if [ $(id -u) = 0 ]; then
-   echo "I have detected that u installed this script as ROOT-User which is a bad idea! This is only a test setup. Go talk to your trusted IoT-advisor of choice!" 
+   echo "I have detected that you installed this script as ROOT-User which is a bad idea! This is only a test setup. Go talk to your trusted IoT-advisor of choice!" 
 fi
 echo "All Done, Portainer is up and running. Visite http://$(hostname -I | cut -d' ' -f1):9000"
