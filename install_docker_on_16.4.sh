0f081ca29125e5dfe14f8ecd7ffb2d5df055a52f
Jan Macenka <36518907+jmacenka@users.noreply.github.com>
2020-09-16 11:02:52 +0200

    Update install_docker_on_16.4.sh

--- install_docker_on_16.4.sh
+++ install_docker_on_16.4.sh
@@ -1,9 +1,12 @@
 echo "Trying to install docker, requires root-privileges and will add current user to docker-group."
-bash <(curl https://get.docker.com)
-eval "sudo usermod -aG docker $(whoami)"
+curl -sSL https://get.docker.com | sh
+sudo usermod -aG $USER
 sudo systemctl start docker
 echo "Installed docker: $(docker --version)"
+echo "Installing required dependencies."
+sudo apt-get install -y libffi-dev libssl-dev
+sudo apt-get install -y python3 python3-pip
+sudo apt-get remove python-configparser
 echo "Trying to install docker-compose."
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo pip3 -v install docker-compose
 echo "Installed docker-compose: $(docker-compose --version)"
